b6ff3128ae6c4a8bb989faa90ce76eddf63f29cc
Zach Nugent <zachary.nugent@usewalter.com>
2023-08-14 16:42:00 -0400

    add react navigation and react-native-webview w/html

--- App.js
+++ App.js
@@ -1,10 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { WebView } from "react-native-webview";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen
+          name="ModalWebView"
+          component={ModalWebView}
+          options={{ presentation: "modal" }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+function ModalWebView() {
+  return <WebView source={{ html: "<h1>Hello world</h1>" }} />;
+}
+
+function Home({ navigation }) {
+  return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TouchableOpacity
+        onPress={() => {
+          navigation.navigate("ModalWebView");
+        }}
+      >
+        <Text>Open modal WebView</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +43,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
